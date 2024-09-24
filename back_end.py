fb0eda1940179908d1daa0c85e82957780749b70
DezyO <145566704+DezyO@users.noreply.github.com>
2024-09-24 13:50:13 +0200

    Flere tall

--- back_end.py
+++ back_end.py
@@ -1,18 +1,17 @@
 def leggSammen():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
-    sum = int(tall1) + int(tall2)
-    print(f'{tall2} + {tall2} = {sum}')
+    tall3 = input("Skriv inn det tredje tallet: ")
+    tall4 = input("Skriv inn det fjerde tallet: ")
+    sum = int(tall1) + int(tall2) + int(tall3) + int(tall4)
+    print(f'{tall2} + {tall2} + {tall3} + {tall4} = {sum}')
 
 
 def trekkFra():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) - int(tall2)
-    print(f'{tall2} - {tall2} = {diff}')
+    tall3 = input("Skriv inn det tredje tallet: ")
+    tall4 = input("Skriv inn det fjerde tallet: ")
+    diff = int(tall1) - int(tall2) - int(tall3) - int(tall4)
+    print(f'{tall2} - {tall2} - {tall3} - {tall4} = {diff}')
 
-
-''' Her er noen påbegynte tanker til utvidelse
-def gange():
-    produkt = 3*3
-    TODO: Gjøre ferdig '''
