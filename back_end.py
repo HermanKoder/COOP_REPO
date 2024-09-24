af4fd392932b10d521ac4ada74338b747a5a18f8
DezyO <145566704+DezyO@users.noreply.github.com>
2024-09-24 14:07:17 +0200

    W in da chat

--- back_end.py
+++ back_end.py
@@ -15,3 +15,18 @@ def trekkFra():
     diff = int(tall1) - int(tall2) - int(tall3) - int(tall4)
     print(f'{tall2} - {tall2} - {tall3} - {tall4} = {diff}')
 
+def gange():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    tall3 = input("Skriv inn det tredje tallet: ")
+    tall4 = input("Skriv inn det fjerde tallet: ")
+    add = int(tall1) * int(tall2) * int(tall3) * int(tall4)
+    print(f'{tall2} * {tall2} * {tall3} * {tall4} = {add}')
+
+def dele():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    tall3 = input("Skriv inn det tredje tallet: ")
+    tall4 = input("Skriv inn det fjerde tallet: ")
+    dra = int(tall1) / int(tall2) / int(tall3) / int(tall4)
+    print(f'{tall2} / {tall2} / {tall3} / {tall4} = {dra}')
